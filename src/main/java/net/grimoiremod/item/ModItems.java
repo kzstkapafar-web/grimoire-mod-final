d249481375e03dd8080bcddb50d11f93ac11f658
kzstkapafar-web <kz.st.kapafar@gmail.com>
2026-08-04 00:35:30 +0300

    Update ModItems.java

--- src/main/java/net/grimoiremod/item/ModItems.java
+++ src/main/java/net/grimoiremod/item/ModItems.java
@@ -7,19 +7,17 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, GrimoireMod.MODID);
 
     public static final RegistryObject<Item> RUNE_FIRE = ITEMS.register("rune_fire",
             () -> new RuneItem(new Item.Properties().stacksTo(64), RuneItem.RuneType.FIRE));
-
     public static final RegistryObject<Item> RUNE_ICE = ITEMS.register("rune_ice",
             () -> new RuneItem(new Item.Properties().stacksTo(64), RuneItem.RuneType.ICE));
-
     public static final RegistryObject<Item> RUNE_LIGHTNING = ITEMS.register("rune_lightning",
             () -> new RuneItem(new Item.Properties().stacksTo(64), RuneItem.RuneType.LIGHTNING));
-
+    public static final RegistryObject<Item> RUNE_CURSE = ITEMS.register("rune_curse",
+            () -> new RuneItem(new Item.Properties().stacksTo(64), RuneItem.RuneType.CURSE));
     public static final RegistryObject<Item> GRIMOIRE_BOOK = ITEMS.register("grimoire_book",
             () -> new GrimoireItem(new Item.Properties().stacksTo(1)));
 }
