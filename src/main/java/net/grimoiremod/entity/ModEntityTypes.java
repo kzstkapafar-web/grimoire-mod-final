b33d5faaa8e397657d19ef0d1392dada30b91790
kzstkapafar-web <kz.st.kapafar@gmail.com>
2026-08-04 00:35:01 +0300

    Update ModEntityTypes.java

--- src/main/java/net/grimoiremod/entity/ModEntityTypes.java
+++ src/main/java/net/grimoiremod/entity/ModEntityTypes.java
@@ -1,18 +1,18 @@
 package net.grimoiremod.entity;
 
 import net.grimoiremod.GrimoireMod;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.registries.DeferredHolder;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModEntityTypes {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
-            DeferredRegister.create(Registries.ENTITY_TYPE, GrimoireMod.MODID);
+            DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, GrimoireMod.MODID);
 
-    public static final DeferredHolder<EntityType<?>, EntityType<WraithEntity>> WRAITH =
-            ENTITY_TYPES.register("wraith", () -> EntityType.Builder.of(WraithEntity::new, MobCategory.MONSTER)
+    public static final RegistryObject<EntityType<WraithEntity>> WRAITH = ENTITY_TYPES.register("wraith",
+            () -> EntityType.Builder.of(WraithEntity::new, MobCategory.MONSTER)
                     .sized(0.6F, 1.95F)
                     .clientTrackingRange(8)
                     .build("wraith"));
