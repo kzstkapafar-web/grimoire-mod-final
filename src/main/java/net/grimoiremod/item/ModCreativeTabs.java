02f486c3f1d1b5b23e4061041ebf2d62b6571487
kzstkapafar-web <kz.st.kapafar@gmail.com>
2026-08-03 22:01:46 +0300

    Update ModCreativeTabs.java

--- src/main/java/net/grimoiremod/item/ModCreativeTabs.java
+++ src/main/java/net/grimoiremod/item/ModCreativeTabs.java
@@ -4,13 +4,13 @@ import net.grimoiremod.GrimoireMod;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModCreativeTabs {
 
-    public static final DeferredRegister<CreativeModeTab> TABS =
-            DeferredRegister.create(ForgeRegistries.CREATIVE_MODE_TABS, GrimoireMod.MODID);
+public static final DeferredRegister<CreativeModeTab> TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, GrimoireMod.MODID);
 
     public static final RegistryObject<CreativeModeTab> GRIMOIRE_TAB = TABS.register("grimoire_tab",
             () -> CreativeModeTab.builder()
