d87fb53b65a6dedfd113cc48b4d46bbe395e5414
kzstkapafar-web <kz.st.kapafar@gmail.com>
2026-08-04 05:49:59 +0300

    Update WraithRenderer.java

--- src/main/java/net/grimoiremod/entity/client/WraithRenderer.java
+++ src/main/java/net/grimoiremod/entity/client/WraithRenderer.java
@@ -2,19 +2,17 @@ package net.grimoiremod.entity.client;
 
 import net.grimoiremod.GrimoireMod;
 import net.grimoiremod.entity.WraithEntity;
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-public class WraithRenderer extends MobRenderer<WraithEntity, HumanoidModel<WraithEntity>> {
+public class WraithRenderer extends MobRenderer<WraithEntity, NecromancerModel> {
 
     private static final ResourceLocation TEXTURE =
             ResourceLocation.fromNamespaceAndPath(GrimoireMod.MODID, "textures/entity/wraith.png");
 
     public WraithRenderer(EntityRendererProvider.Context context) {
-        super(context, new HumanoidModel<>(context.bakeLayer(ModelLayers.ZOMBIE)), 0.5F);
+        super(context, new NecromancerModel(context.bakeLayer(NecromancerModel.LAYER_LOCATION)), 0.5F);
     }
 
     @Override
